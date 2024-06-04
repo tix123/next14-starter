8a7d5a646e09b184087e3ee7c86268363356b46e
Sean Chen <tix123@msn.com>
2024-06-04 20:51:08 +0800

    Complete

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -4,8 +4,11 @@ import AdminPosts from "@/components/adminPosts/adminPosts";
 import AdminPostForm from "@/components/adminPostForm/adminPostForm";
 import AdminUserForm from "@/components/adminUserForm/adminUserForm";
 import AdminUsers from "@/components/adminUsers/adminUsers";
+import { auth } from "@/lib/auth";
 
-const AdminPage = () => {
+const AdminPage = async () => {
+    const session = await auth();
+
     return (
         <div className={styles.container}>
             <div className={styles.row}>
@@ -15,7 +18,7 @@ const AdminPage = () => {
                     </Suspense>
                 </div>
                 <div className={styles.col}>
-                    <AdminPostForm />
+                    <AdminPostForm userId={session.user.id} />
                 </div>
             </div>
             <div className={styles.row}>
--- src/components/adminPostForm/adminPostForm.jsx
+++ src/components/adminPostForm/adminPostForm.jsx
@@ -1,7 +1,24 @@
-import styles from "./adminPostForm.module.css";
+"use client";
 
-const AdminPostForm = () => {
-    return <div className={styles.container}></div>;
+import styles from "./adminPostForm.module.css";
+import { addPost } from "@/lib/action";
+import { useFormState } from "react-dom";
+
+const AdminPostForm = ({ userId }) => {
+    const [state, formAction] = useFormState(addPost, undefined);
+
+    return (
+        <form action={formAction} className={styles.container}>
+            <h1>Add New Post</h1>
+            <input type="hidden" name="userId" value={userId} />
+            <input type="text" name="title" placeholder="Title" />
+            <input type="text" name="slug" placeholder="slug" />
+            <input type="text" name="image" placeholder="image" />
+            <textarea type="text" name="desc" placeholder="desc" rows={10} />
+            <button>Add</button>
+            {state && state.error}
+        </form>
+    );
 };
 
 export default AdminPostForm;
